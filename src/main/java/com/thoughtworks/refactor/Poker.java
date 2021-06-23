2b4053adab16a412e03912659504e459e98fe339
LouisTang <louis.tang@cargosmart.com>
2021-06-23 11:26:29 +0800

    renaming some vars in judgeCategoty

--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -254,36 +254,36 @@ public class Poker {
     }
 
     //判断是什么牌
-    private String judgeCategoty(String str) {
+    private String judgeCategoty(String hands) {
         String type = "";
-        String[] strArray = str.split("");
-        int[] number = sortNumberDesc(str);
+        String[] strArray = hands.split("");
+        int[] number = sortNumberDesc(hands);
         int i;
         String[] color = new String[5];
         for (i = 0; i < 5; i++) {
             color[i] = strArray[i * 3 + 1];
         }
-        HashSet<Integer> hashSetNumber = new HashSet<Integer>();
+        HashSet<Integer> distinctNumbers = new HashSet<Integer>();
         for (i = 0; i < 5; i++) {
-            hashSetNumber.add(number[i]);
+            distinctNumbers.add(number[i]);
         }
-        HashSet<String> hashSetType = new HashSet<String>();
+        HashSet<String> suits = new HashSet<String>();
         for (i = 0; i < 5; i++) {
-            hashSetType.add(color[i]);
+            suits.add(color[i]);
         }
-        if (hashSetNumber.size() == 5) {
-            if ((number[0] - number[4] == 4) && (hashSetType.size() == 1) && (hashSetNumber.size() == 5)) { //五个相邻的数字且花色一样——同花顺
+        if (distinctNumbers.size() == 5) {
+            if ((number[0] - number[4] == 4) && (suits.size() == 1) && (distinctNumbers.size() == 5)) { //五个相邻的数字且花色一样——同花顺
                 type = "StraightFlush";
-            } else if (number[0] - number[4] == 4 && (hashSetNumber.size() == 5)) { //五个相邻数字——顺子
+            } else if (number[0] - number[4] == 4 && (distinctNumbers.size() == 5)) { //五个相邻数字——顺子
                 type = "Straight";
-            } else if (hashSetType.size() == 1) { //同一花色——同花
+            } else if (suits.size() == 1) { //同一花色——同花
                 type = "Flush";
             } else { //五个不相邻的数字——散牌
                 type = "HighCard";
             }
-        } else if (hashSetNumber.size() == 4) { //一对相同，其余三个数字不同——对子
+        } else if (distinctNumbers.size() == 4) { //一对相同，其余三个数字不同——对子
             type = "OnePair";
-        } else if (hashSetNumber.size() == 3) {
+        } else if (distinctNumbers.size() == 3) {
             if ((number[0] == number[1] && number[2] == number[3]) || (number[1] == number[2] && number[3] == number[4]) || (number[0] == number[1] && number[3] == number[4])) { //两对
                 type = "TwoPair";
             } else { //三个数字相同，另外两个数字不同——三条
